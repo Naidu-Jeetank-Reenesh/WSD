f9819aa0a08c6401c06866d91e5477ba8e5ed612
Naidu-Jeetank-Reenesh <95245121+Naidu-Jeetank-Reenesh@users.noreply.github.com>
2022-02-17 01:46:14 +0530

    Delete nodejson.js

--- nodejson.js
+++
@@ -1,5 +0,0 @@
-const {readFileSync, read, readFile} = require('fs')
-var loadjson = ()=>JSON.parse(readFileSync('staff.json'))
-var data = readFileSync('staff.json')
-console.log(JSON.parse(data)['staff'][0])
-module.exports = {loadjson};
